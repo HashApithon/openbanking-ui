9e6276e5c360442f0c4f1635ed8ffac94d796040
HashApithon <67086333+HashApithon@users.noreply.github.com>
2020-07-04 23:40:57 +0530

    Added bootstrap and updated looks (#5)
    Co-authored-by: frictionlessdev <shivibh@gmail.com>

--- packages/ui-common/lib/Header.js
+++ packages/ui-common/lib/Header.js
@@ -7,8 +7,12 @@ exports["default"] = Header;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./common.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function Header() {
-  return /*#__PURE__*/_react["default"].createElement("div", null, "hello");
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "header"
+  }, /*#__PURE__*/_react["default"].createElement("h2", null, "#APIthon"));
 }
