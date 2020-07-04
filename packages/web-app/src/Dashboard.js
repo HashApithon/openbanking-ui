558043b3dcb82c3494b3786ade9c813c12189ec1
frictionlessdev <shivibh@gmail.com>
2020-07-04 23:35:18 +0530

    Added bootstrap and updated looks

--- packages/web-app/src/Dashboard.js
+++ packages/web-app/src/Dashboard.js
@@ -3,13 +3,35 @@ import { connect } from 'react-redux';
 
 function Dashboard(props) {
     return (
+        // <div className="dashboard">
+        //     <div className="text">What would you like to choose?</div>
+        //     <div className="journeyBtns">
+        //         <button className="journeyBtn">Account APIs</button>
+        //         <button className="journeyBtn">Payment APIs</button>
+        //     </div>
+        // </div>
         <div className="dashboard">
-            <div className="text">What would you like to choose?</div>
-            <div className="journeyBtns">
-                <button className="journeyBtn">Account APIs</button>
-                <button className="journeyBtn">Payment APIs</button>
-            </div>
+        <div class="container">
+        <div class="row">
+          
+          <div class="col-sm">
+              
+            <div className="text"><h3>Open Banking</h3></div>
         </div>
+
+        </div>
+        <div class="row">
+          
+          <div class="col-sm">
+              
+            <div className="journeyBtns">
+                 <button className="journeyBtn">Account Information Service Provider (AISP)</button>
+                 <button className="journeyBtn">Payment Initiation Service Provider (PISP)</button>
+             </div>
+          </div>
+        </div>
+      </div>
+      </div>
     )
 }
 
