65c85a2119027fe58772960b532538c0f569d148
Soni Goyal <sonigoyal.jd@gmail.com>
2020-07-06 13:23:46 +0530

    remove code warnings

--- packages/web-app/src/App.js
+++ packages/web-app/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from 'react'
 import { Route, BrowserRouter } from 'react-router-dom'
-import Header from '@openbanking/ui-common/lib/Header';
+import Header from '@openbanking/ui-common/lib/Header'
 import ErrorBoundary from '@openbanking/ui-common/lib/Error'
-import Loading from '@openbanking/ui-common/lib/Loading';
+import Loading from '@openbanking/ui-common/lib/Loading'
 import Home from './Home'
 import Dashboard from './Dashboard'
-import './App.css';
-
+import './App.css'
 
 function App(props) {
-  return (
-    <ErrorBoundary>
-      <div className="app">
-        <Header />
-        <div className="section-content">
-          <BrowserRouter >
-            <Route path="/" component={Dashboard} />
-            <Route path="/home" component={Home} />
-          </BrowserRouter>
-        </div>
-      </div>
-    </ErrorBoundary>
-  );
+    return (
+        <ErrorBoundary>
+            {props.loading && <Loading />}
+            <div className="app">
+                <Header />
+                <div className="section-content">
+                    <BrowserRouter>
+                        <Route path="/" component={Dashboard} />
+                        <Route path="/home" component={Home} />
+                    </BrowserRouter>
+                </div>
+            </div>
+        </ErrorBoundary>
+    )
 }
 
-export default App;
+export default App
--- packages/web-app/src/Home.js
+++ packages/web-app/src/Home.js
@@ -1,32 +1,31 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import createRequest from '@openbanking/ui-data/src/services/request'
 
-import { connect } from 'react-redux';
-import Loading from '@openbanking/ui-common/lib/Loading';
+import { connect } from 'react-redux'
+import Loading from '@openbanking/ui-common/lib/Loading'
 
 function Home(props) {
-  useEffect(()=>{
     props.getInitialData()
-  },[])
-  return (
-    <div>
-      This is {props.message} from redux store
-      <button onClick={() => props.getInitialData('Dispatch hello')}>Click Me</button>
-     Home component 
-     <Loading />
-     <pre>{(JSON.stringify(props.data,null,4))}</pre>
-    </div>
-  );
+
+    return (
+        <div>
+            {/* This is {props.message} from redux store */}
+            <button onClick={() => props.getInitialData('Dispatch hello')}>
+                Click Me
+            </button>
+            Home component
+            <Loading />
+            <pre>{JSON.stringify(props.data, null, 4)}</pre>
+        </div>
+    )
 }
 
-const mapStateToProps = state => ({
-    data: state.app.data
-});
+const mapStateToProps = (state) => ({
+    data: state.app.data,
+})
 
+const mapDispatchToProps = (dispatch) => ({
+    getInitialData: () => createRequest(dispatch, 'todos/1', 'get'),
+})
 
-const mapDispatchToProps = dispatch => ({
-    getInitialData:()=>
-        createRequest(dispatch,'todos/1','get')
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
