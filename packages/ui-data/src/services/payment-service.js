9d67b8b3e8913a28253ab10308f583390d5e4243
varsha agnihotri <agnihotrivarsha@gmail.com>
2020-07-12 01:57:41 +0530

    OB: Added higher level PISP flow

---
+++ packages/ui-data/src/services/payment-service.js
@@ -0,0 +1,28 @@
+import createRequest from './request'
+import { setData } from '../actions/app'
+
+export function createDomesticPayment(dispatch, payload) {
+    createRequest(
+        dispatch,
+        '/domestic-payments',
+        'POST',
+        { payload },
+        {},
+        function (response) {
+            dispatch(setData(response))
+        }
+    )
+}
+
+export function getDomesticPaymentStatus(dispatch) {
+    createRequest(
+        dispatch,
+        '/domestic-payment-status',
+        'GET',
+        null,
+        {},
+        function (response) {
+            dispatch(setData(response))
+        }
+    )
+}
