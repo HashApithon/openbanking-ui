834468ae3ce14835c8b0f75db42bfd813f2b44a0
varsha agnihotri <agnihotrivarsha@gmail.com>
2020-07-11 17:19:10 +0530

    OB: Reverting redux hook here (need to check for multiple token calls with hooks)

--- packages/web-app/src/Redirecting.js
+++ packages/web-app/src/Redirecting.js
@@ -1,22 +1,29 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { parse } from 'query-string'
-import { Redirect } from 'react-router'
+import { connect } from 'react-redux'
 import { getAccessToken } from '@openbanking/ui-data/lib/services/auth-service'
 import Loading from '@openbanking/ui-common/lib/Loading'
+import { parse } from 'query-string'
+import { Redirect } from 'react-router'
 
-const Redirecting = () => {
-    const journeyType = useSelector((state) => sessionStorage.getItem('type'))
-    const dispatch = useDispatch()
-
+function Redirecting(props) {
     // dont allow user to access this page directly
-    if (!journeyType) {
+    if (!props.journeyType) {
         return <Redirect to="/" />
     } else {
-        const { code } = parse(window.location.hash.substring(1))
-        getAccessToken(dispatch, code, journeyType)
+        if (props.journeyType) {
+            const { code } = parse(window.location.hash.substring(1))
+            props.getAccessToken(code, props.journeyType)
+        }
     }
     return <Loading />
 }
 
-export default Redirecting
+const mapStateToProps = (state) => ({
+    journeyType: sessionStorage.getItem('type'),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    getAccessToken: (code, type) => getAccessToken(dispatch, code, type),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Redirecting)
